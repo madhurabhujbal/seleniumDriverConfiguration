9c884a05436d9144e80270770edc10b9fdcabf03
Madhura <madhura.bhujbal@gmail.com>
2020-10-10 13:49:20 +0100

    Store chrome browser options in a constant

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Builder } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
+const options = new chrome.Options();
 
 const driver = new Builder().forBrowser("chrome").build();
 
