12c38c1f9d519a2d7cbb9a97d3cf941f4dfb7950
Madhura <madhura.bhujbal@gmail.com>
2020-10-10 14:08:09 +0100

    Add code to instruct browser to download application csv file everytime

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const options = new chrome.Options();
 
 options.setPreference("browser.download.dir", "C:\\mySeleniumDownloads");
 options.setPreference("browser.download.folderList", 2);
+options.setPreference(
+  "browser.helperApps.neverAsk.saveToDisk",
+  "application/x-csv"
+);
 
 const driver = new Builder().forBrowser("chrome").build();
 
