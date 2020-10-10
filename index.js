601982c2bfe3aa203aa57be0f559e954ea362875
Madhura <madhura.bhujbal@gmail.com>
2020-10-10 14:05:15 +0100

    Set destination to download files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const chrome = require("selenium-webdriver/chrome");
 const options = new chrome.Options();
 
 options.setPreference("browser.download.dir", "C:\\mySeleniumDownloads");
+options.setPreference("browser.download.folderList", 2);
 
 const driver = new Builder().forBrowser("chrome").build();
 
