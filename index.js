395cdfa0de78347425745192850ffa51893d11e8
Madhura <madhura.bhujbal@gmail.com>
2020-10-10 14:04:17 +0100

    Set preferences to download file at specific destination

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { Builder } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const options = new chrome.Options();
 
+options.setPreference("browser.download.dir", "C:\\mySeleniumDownloads");
+
 const driver = new Builder().forBrowser("chrome").build();
 
 driver.get("http://google.com");
