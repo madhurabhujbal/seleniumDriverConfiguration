19e6ab8ac7d1fe720ab9f05207a7762d1c24fe0d
Madhura <madhura.bhujbal@gmail.com>
2020-10-14 11:12:17 +0100

    Add path for new chrome setup with extensions provided

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const { Builder } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const options = new chrome.Options();
 
-options.setUserPreferences({
-  "download.default_directory":
-    "C:\\selenium_projects\\driverConfiguration\\delme",
-});
-options.setUserPreferences({
-  "browser.helperApps.neverAsk.saveToDisk": "application/x/csv",
-});
+options.addArguments(
+  "user-data-dir=C:\\selenium_projects\\driverConfiguration\\chrome-dev-profile"
+);
+
+// options.setUserPreferences({
+//   "download.default_directory":
+//     "C:\\selenium_projects\\driverConfiguration\\delme",
+// });
+// options.setUserPreferences({
+//   "browser.helperApps.neverAsk.saveToDisk": "application/x/csv",
+// });
 
 const driver = new Builder()
   .forBrowser("chrome")
   .setChromeOptions(options)
   .build();
 
-driver.get("https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv");
+// driver.get("https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv");
+driver.get("https://www.google.com");
