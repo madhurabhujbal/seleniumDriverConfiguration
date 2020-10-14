5451cf96618d63f53b501921d66536f35004ca73
Madhura <madhura.bhujbal@gmail.com>
2020-10-14 11:12:52 +0100

    Delete the unwanted code

--- index.js
+++ index.js
@@ -6,18 +6,9 @@ options.addArguments(
   "user-data-dir=C:\\selenium_projects\\driverConfiguration\\chrome-dev-profile"
 );
 
-// options.setUserPreferences({
-//   "download.default_directory":
-//     "C:\\selenium_projects\\driverConfiguration\\delme",
-// });
-// options.setUserPreferences({
-//   "browser.helperApps.neverAsk.saveToDisk": "application/x/csv",
-// });
-
 const driver = new Builder()
   .forBrowser("chrome")
   .setChromeOptions(options)
   .build();
 
-// driver.get("https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv");
 driver.get("https://www.google.com");
